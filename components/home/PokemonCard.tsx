cf0d9a25dcca95002817b132da44699535395407
Om Londhe <oplondhe@gmail.com>
2022-03-11 23:42:44 +0530

    After designing whole card

--- components/home/PokemonCard.tsx
+++ components/home/PokemonCard.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { POKEMON_DATA_URL } from "../../services/constants";
+import { BACKGROUND_COLOR, POKEMON_DATA_URL } from "../../services/constants";
 import { Pokemon, PokemonExtraData } from "../../services/types";
 import pokemonCardStyles from "../../styles/components/home/PokemonCard.module.css";
 
@@ -18,25 +18,31 @@ const PokemonCard: ({ id, name, images, no }: Pokemon) => JSX.Element = ({
       .then((response) => setData(response.data));
   }, []);
 
-  console.log("data", data);
-  console.log(no);
   console.log(data?.types);
-  console.log(data?.height);
-  console.log(data?.weight);
-  console.log();
 
   return (
     <div className={pokemonCardStyles.card}>
       <p className={pokemonCardStyles.initial}>{name.substring(0, 1)}</p>
-      <div className={pokemonCardStyles.cardData}>
+      <div
+        className={pokemonCardStyles.cardData}
+        style={{
+          backgroundColor: data?.types
+            ? BACKGROUND_COLOR[data?.types[0].type.name]
+            : "white",
+        }}
+      >
         <img src={images.main} alt={name} className={pokemonCardStyles.image} />
         <div className={pokemonCardStyles.imageShadow}></div>
         <p className={pokemonCardStyles.name}>{name}</p>
-        <p className={pokemonCardStyles.height}>
-          {data?.height ? data?.height * 10 : 0} cm
-        </p>
-        <p className={pokemonCardStyles.height}>{data?.weight} kg</p>
-        <p className={pokemonCardStyles.height}>EXP:{data?.base_experience}</p>
+        <div className={pokemonCardStyles.heightAndWeight}>
+          <p className={pokemonCardStyles.height}>
+            Height: {data?.height ? data?.height * 10 : 0} cm
+          </p>
+          <p className={pokemonCardStyles.weight}>
+            Weight: {data?.weight ? data?.weight / 10 : data?.weight} kg
+          </p>
+        </div>
+        <p className={pokemonCardStyles.xp}>{data?.base_experience}</p>
       </div>
     </div>
   );
--- services/constants.ts
+++ services/constants.ts
@@ -5,3 +5,26 @@ export const POKEMON_MAIN_IMAGE =
   "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/";
 export const POKEMON_OFFICIAL_IMAGE =
   "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/";
+
+export const BACKGROUND_COLOR: { [key: string]: string } = {
+  normal: "pink",
+  fighting: "purple",
+  flying: "indigo",
+  poison: "darkblue",
+  ground: "brown",
+  rock: "grey",
+  bug: "copper",
+  ghost: "#303030",
+  steel: "silver",
+  fire: "orange",
+  water: "skyblue",
+  grass: "green",
+  electric: "yellow",
+  psychic: "magenta",
+  ice: "lightblue",
+  dragon: "coral",
+  dark: "black",
+  fairy: "wheat",
+  unknown: "white",
+  shadow: "#444444",
+};
